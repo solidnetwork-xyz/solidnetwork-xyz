3bd6b34078dc66ba3abb087ee7db6f8c837b476f
solidnetwork-xyz <2281302+solidnetwork-xyz@users.noreply.github.com>
2026-06-21 02:09:22 -0400

    Add mock response data for additional worker

--- solidnetwork.xyz/src/store/mock/response/payload.js
+++ solidnetwork.xyz/src/store/mock/response/payload.js
@@ -7,6 +7,13 @@
       "name": "George Maxwell",
       "qr": "data:image/png;base,aW1hZ2Uvbi1iZDRmZTM2YTIxL2dlb3JnZS5tYXh3ZQ==",
       "factor": "data:image/png;base64,aW1hZ2Uvbi00NDY1NDUxMjkwNDUxODM1L2dlb3JnZS5tYXh3ZWxs"
+    },
+    {
+      "last": "15-019ee8c2-ef82-7db0-93df-961a42125429",
+      "email": "gh@example.com",
+      "name": "Gerald Hill",
+      "qr": "data:image/png;base,aW1hZ2Uvbi1iZDRmZTM2YTIxL2dlb3JnZS5tYXh3ZQ==",
+      "factor": "data:image/png;base64,aW1hZ2UvMTAtNThiMDAtN2M3YzQ3ODI0YS1jMGYzM2Y5Yjc1LWU3OGYzMTU0ZmMtYWIzMmMvZ2VyYWxkLmhpbGw="
     }
   ]
 }
